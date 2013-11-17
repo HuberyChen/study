c6e4afe5f55be444bd892dccda799157353ce943
HuberyChen <embracelong@sina.com>
2013-11-17 23:50:06 +0800

    home

---
+++ exam/src/Exception/Construction/Sub.java
@@ -0,0 +1,17 @@
+package Exception.Construction;
+
+import java.io.IOException;
+
+/**
+ * @author hubery.chen
+ * @date (2013-11-17 23:40)
+ */
+public class Sub extends Base {
+
+    public Sub() throws Exception {
+
+    }
+
+    @Override
+    public void method() throws IOException {}
+}
