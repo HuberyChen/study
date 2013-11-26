862de003a9e20c3c694f975a689421f55308e332
HuberyChen <embracelong@sina.com>
2013-11-26 22:27:24 +0800

    home

---
+++ exam/src/Exception/Throwable/Foo.java
@@ -0,0 +1,19 @@
+package Exception.Throwable;
+
+/**
+ * @author hubery.chen
+ * @date (2013-11-18 22:53)
+ */
+public class Foo {
+
+    public static boolean m() {
+        StackTraceElement[] sts = new Throwable().getStackTrace();
+        for (StackTraceElement st : sts) {
+            if (st.getClassName().equals("m1")) {
+                return true;
+            }
+        }
+        throw new RuntimeException("In addition to the m1 method, this method does not allow other method calls");
+    }
+
+}
